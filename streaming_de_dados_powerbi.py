7021b6c96729deab9eb9c56e7080587ea23f4930
Marcelo Monier Jr <marcelomonier@gmail.com>
2022-11-29 23:15:04 -0300

    Alterações no json do clima

--- streaming_de_dados_powerbi.py
+++ streaming_de_dados_powerbi.py
@@ -1,23 +1,39 @@
+# importing library
 import requests
-import random
-import datetime
-import time
+from bs4 import BeautifulSoup
 
-print("Streaming de Dados - Power bi")
 
-URL = "https://api.powerbi.com/beta/1096f5e7-4729-42f2-a640-2ee479abdd16/datasets/a85ee10a-94cd-40e0-b03b-c5f8dad34841/rows?responseError=UserNotLicensedru%3Dhttps%3A%2F%2Fapp.powerbi.com%2F%3FresponseError%3DUserNotLicensedru%253Dhttps%253A%252F%252Fapp.powerbi.com%252Fgroups%252Fme%252Fapps%253FrefreshAccessToken%253Dtrue%26pbi_source%3Dweb_nolicense_redirect%26redirectedFromSignup%3D1%26noSignUpCheck%3D1%26ScenarioId%3DSignup&redirectedFromSignup=1&redirectedWaitSimple=1&key=cd%2F5UMKL99ykz8WjKyINsvQuEefe52nAqa85PwEUdmou3NecxFqKUwHZKktuhjDakjDtX6n3ywj9Do6EayNPMg%3D%3D"
 
-data = []
+# enter city name
+city = "São Luís"
 
-for i in range(150):
-	data = []
-	datahora = datetime.datetime.now()
-	datahoraformatada = datahora.strftime("%Y-%m-%d %H:%M:%S")
-	temp = random.randint(30,40)
-	umidade = random.randint(10,70)
-	registro = { "Data": datahoraformatada, "Temperatura": temp, "UmidadeRelativa": umidade}
-	data.append(registro)
-	requests.post(url = URL, json = data)
-	time.sleep(0.2)
+# creating url and requests instance
+url = "https://www.google.com/search?q="+"weather"+city
+html = requests.get(url).content
 
-print(data)
+# getting raw data
+soup = BeautifulSoup(html, 'html.parser')
+temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+
+# formatting data
+data = str.split('\n')
+time = data[0]
+sky = data[1]
+data2 = temp.split('\n')
+temp3 = data2[2]
+
+# getting all div tag
+listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+strd = listdiv[5].text
+
+# getting other required data
+pos = strd.find('Wind')
+other_data = strd[pos:]
+
+# printing all data
+print('\n', city, '\n')
+print("Temperatura: ", temp3)
+print("Data: ", time)
+print("Descrição do céu: ", sky)
+print(other_data)
